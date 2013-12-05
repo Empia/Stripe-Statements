9df35e37c2b06b47b56ccdfabb237faf66f76a8e
Keith Harris <keith@keithharris.org>
2013-12-05 17:24:24 +0000

    Connecting through to Stripe

--- src/main/java/com/softlysoftware/stripestatements/Main.java
+++ src/main/java/com/softlysoftware/stripestatements/Main.java
@@ -2,9 +2,28 @@ package com.softlysoftware.stripestatements;
 
 import java.util.Properties;
 import java.util.Date;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Calendar;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import com.stripe.Stripe;
+import com.stripe.model.BalanceTransaction;
+import com.stripe.model.BalanceTransactionCollection;
+import com.stripe.exception.StripeException;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.exception.APIConnectionException;
+import com.stripe.exception.APIException;
+import com.stripe.exception.CardException;
 
 public class Main {
 
@@ -37,6 +56,7 @@ public class Main {
         };
 		File[] extant = directory.listFiles(fileFilter);
 		int sequence = 0;
+		Set<String> seenTransactions = new HashSet<String>();
 		if (extant.length == 0) {
 			log("No existing statement files found. Assuming this is the first run.");
 			sequence = 1;
@@ -53,10 +73,50 @@ public class Main {
 					log("ABORT : Bad statement file name : " + name);
 					return;
 				}
+				try {
+					BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(extant[i]), "UTF-8"));
+					String line = in.readLine();
+					while ((line = in.readLine()) != null) {
+						String[] column = line.split("\t");
+						seenTransactions.add(column[0]);
+					}
+					in.close();
+				}
+				catch (UnsupportedEncodingException e) {throw new RuntimeException("Should never happen.", e);}
+				catch (FileNotFoundException e) {throw new RuntimeException("Should never happen.", e);}
+				catch (IOException e) {throw new RuntimeException("Problem while reading file.", e);}
 			}
 		}
+		log("Scanned " + seenTransactions.size() + " transactions.");
 		File statement = new File(directory, prefix + sequence + suffix);
 		log("Next statement file will be : " + statement);
+		String stripeSecret = properties.getProperty("stripe.secret");
+		log("Property : stripe.secret : " + String.format("%" + stripeSecret.length() + "s", "").replace(' ', '*'));
+		int days = Integer.parseInt(properties.getProperty("stripe.days"));
+		log("Property : stripe.days : " + days);
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, days * -1);
+		log("Getting transactions since : " + cal.getTime());
+		Stripe.apiKey = stripeSecret;
+		Map<String, Object> params = new HashMap<String, Object>();
+		Map<String, Object> sub = new HashMap<String, Object>();
+		sub.put("gt", cal.getTime().getTime()/1000);
+		params.put("created", sub);
+		try {
+			BalanceTransactionCollection balanceTransactionCollection = BalanceTransaction.all(params);
+			log("Got " + balanceTransactionCollection.getCount() + " transactions from Stripe");
+			for (BalanceTransaction transaction : balanceTransactionCollection.getData()) {
+
+			}
+		}
+		catch (AuthenticationException e) {
+			log("ABORT : Authentication problems connecting to Stripe. Please check your secret in the properties file.");
+			return;
+		}
+		catch (InvalidRequestException e) {throw new RuntimeException(e);}
+		catch (APIException e) {throw new RuntimeException(e);}
+		catch (APIConnectionException e) {throw new RuntimeException(e);}
+		catch (CardException e) {throw new RuntimeException(e);}
 		log("Stripe Statements ended at : " + new Date());
 	}
 
